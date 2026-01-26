b72875365d49587706f2fd4d519076d4384459e4
richardl22 <richardl22@khu.ac.kr>
2026-01-26 11:43:05 +0900

    [FEAT] Set text of current page buttton orange

--- Unity_RTSP/Assets/01_Script/UI/PageButtonManager.cs
+++ Unity_RTSP/Assets/01_Script/UI/PageButtonManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
 public class PageButtonManager : MonoBehaviour
 {
     [SerializeField] List<Button> pageButtons = new List<Button>();
     [SerializeField] private PageManager pageManager;
+
+    [SerializeField] private Color activeColor = new Color(1f,0.5f,0f);
+    [SerializeField] private Color inactiveColor = Color.black;
 
     private int pageCount = 0;
     public void AddButton()
@@ -23,5 +27,18 @@ public class PageButtonManager : MonoBehaviour
     private void RequestChangePage(int index)
     {
         pageManager.ChangePage(index);
+        SetCurrentPageButtonColor(index);
+    }
+
+    private void SetCurrentPageButtonColor(int index)
+    {
+        for (int i = 0; i < pageButtons.Count; i++)
+        {
+            TMP_Text buttonText = pageButtons[i].GetComponentInChildren<TMP_Text>();
+            if (buttonText != null)
+            {
+                buttonText.color = (i == index) ? activeColor : inactiveColor;
+            }
+        }
     }
 }
